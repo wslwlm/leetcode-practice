822f1caf2c41fdf8f5d537f70e9f60a12d8bc63d
wslwlm <1140873504@qq.com>
2020-01-28 17:00:21 +0800

    5. Longest Palindromic Substring add Manacher algrithm

--- 5. Longest Palindromic Substring/Longest-Palindromic-Substring.py
+++ 5. Longest Palindromic Substring/Longest-Palindromic-Substring.py
@@ -1,5 +1,6 @@
 # 题目地址: https://leetcode.com/problems/longest-palindromic-substring/
 # RunTime: 32ms
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         str_len = len(s)
@@ -25,7 +26,8 @@ class Solution:
         return s[start:start + max_len]
 
 
-# 动态规划的方法
+# 动态规划的方法, dp储存的是i到j这一段是否为回文串, 如果i+1到j-1为回文串且s[i]==s[j] ==> i到j为回文
+# 递推式: dp[i][j] = dp[i+1][j-1] and s[i] == s[j]
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -58,3 +60,73 @@ class Solution(object):
             length += 1
 
         return s[start:end+1]
+
+
+# Manacher算法
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        string_size = len(s)
+        if string_size < 3:
+            if s==s[::-1]:
+                return s
+            else:
+                return s[0]
+
+        manacher_str = "#"
+        for index in range(len(s)):
+            manacher_str += s[index]
+            manacher_str += "#"
+
+        LPS_table = [0]*len(manacher_str)
+        center = 1
+        max_right = 2
+        max_length = 0
+        LPS_center = 0
+
+        total_size = len(manacher_str)
+        for index in range(1, len(manacher_str)):
+            if index < max_right:
+                LPS_table[index] = min(LPS_table[2*center-index], max_right-index)
+            else:
+                LPS_table[index] = 0
+
+            # when calculating LPS value, self position (index) is not included
+            while (index-LPS_table[index]-1 >= 0 and
+                   index+LPS_table[index]+1 < total_size and
+                   manacher_str[index-LPS_table[index]-1] == manacher_str[index+LPS_table[index]+1]):
+                   LPS_table[index] += 1
+
+            if LPS_table[index] > max_length:
+                max_length = LPS_table[index]
+                LPS_center = index
+
+            if LPS_table[index]+index-1 > max_right:
+                max_right = LPS_table[index]+index-1
+                center = index
+
+        start = int((LPS_center-max_length)/2)
+        return s[start: start+max_length]
+
+
+# 通过Manacher算法中的预处理, 只用判断奇数串即可
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        s = '#'.join(s)
+        s = '#' + s + '#'
+        head = 0
+        maxlen = 1
+        for rd_tail in range(2, len(s)):
+            rd_head = rd_tail - maxlen - 1
+            rd_s = s[rd_head: rd_tail + 1]
+            if rd_s == rd_s[::-1] and rd_head >= 0:
+                head = rd_head
+                maxlen += 2
+        return s[head: head + maxlen].replace('#','')
+
+
+# Manacher算法参考链接:
+# https://mp.weixin.qq.com/s/EiJum3-44TqXZN4apuSQUQ
+# https://windliang.cc/2019/06/24/%E4%B8%80%E6%96%87%E8%AE%A9%E4%BD%A0%E5%BD%BB%E5%BA%95%E6%98%8E%E7%99%BD%E9%A9%AC%E6%8B%89%E8%BD%A6%E7%AE%97%E6%B3%95/
+# https://segmentfault.com/a/1190000008484167
--- learndata.md
+++ learndata.md
@@ -13,6 +13,12 @@
 - [x] LIS(最长上升子序列)
 - [x] LCS(最长公共子序列)
 - [x] 二分查找
+- [x] Manacher算法(马拉车算法)
+
+* https://segmentfault.com/a/1190000008484167
+* [https://windliang.cc/2019/06/24/%E4%B8%80%E6%96%87%E8%AE%A9%E4%BD%A0%E5%BD%BB%E5%BA%95%E6%98%8E%E7%99%BD%E9%A9%AC%E6%8B%89%E8%BD%A6%E7%AE%97%E6%B3%95/](https://windliang.cc/2019/06/24/一文让你彻底明白马拉车算法/)
+* https://mp.weixin.qq.com/s/EiJum3-44TqXZN4apuSQUQ
+
 - [x] 回溯法
 
 * https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
